b9a28e4dca8d1655d00d82a61548a59b7113a21d
rawan-sous <103501720+rawan-sous@users.noreply.github.com>
2024-06-07 18:30:50 +0300

    Add files via upload

--- pills.c
+++ pills.c
@@ -10,6 +10,8 @@
 Shared_Argument *shared_args;
 pthread_mutex_t mutex;
 int simulation_running = 1;
+int line_index;
+int production_line;
 
 void generate_pills(Pill_Container *pill) {
     pill->count = rand() % 30 + 1; // Generate pill count between 1 and 30
@@ -33,7 +35,7 @@ int check_validity1(Pill_Container *pill) {
 
 void* production_function1(void *args) {
     int local_line_index = *(int *)args;
-    srand(time(NULL) ^ (local_line_index + getpid()));
+    srand(time(NULL) ^ getpid());
     shared_args->produced_pills[local_line_index] = 0;
     int delay = rand() % (shared_args->MAX_DELAY - shared_args->MIN_DELAY + 1) + shared_args->MIN_DELAY;
 
@@ -54,7 +56,7 @@ void* production_function1(void *args) {
 
 void* inspectors_function1(void *args) {
     int local_line_index = *(int *)args;
-    srand(time(NULL) ^ (local_line_index + getpid()));
+    srand(time(NULL) ^ getpid());
     printf("Inspector function started for line %d\n", local_line_index);
 
     while (simulation_running) {
@@ -69,7 +71,7 @@ void* inspectors_function1(void *args) {
                 if (is_valid) {
                     shared_args->inspected_index[local_line_index][i] = 1;
                     shared_args->produced_index[local_line_index][i] = 0;
-                    printf("Line %d inspected pill container %d and it is valid.\n", local_line_index, i);
+                    printf("Line %d inspected pill container %d\n", local_line_index, i);
                 } else {
                     shared_args->produced_index[local_line_index][i] = 0;
                     printf("Line %d has produced an invalid pill container!\n", local_line_index);
@@ -84,7 +86,7 @@ void* inspectors_function1(void *args) {
 
 void* packagers_function1(void *args) {
     int local_line_index = *(int *)args;
-    srand(time(NULL) ^ (local_line_index + getpid()));
+    srand(time(NULL) ^ getpid());
     shared_args->pills_packaged[local_line_index] = 0;
 
     printf("Packager function started for line %d\n", local_line_index);
@@ -108,21 +110,51 @@ void* packagers_function1(void *args) {
     return NULL;
 }
 
+int find_minimum_speed1() {
+    int min_speed = shared_args->production_speed[0];
+    int min_index = 0;
+
+    for (int i = 1; i < shared_args->NUM_OF_PRODUCTION_LINES; i++) {
+        if (shared_args->production_speed[i] < min_speed) {
+            min_speed = shared_args->production_speed[i];
+            min_index = i;
+        }
+    }
+    return min_index;
+}
+
 void* manager_function1(void *args) {
     int local_line_index = *(int *)args;
+    time_t current_time;
 
     printf("Manager function started for line %d\n", local_line_index);
 
     while (simulation_running) {
-        sleep(10);
+        time(&current_time);
+        if (shared_args->SIMULATION_DURATION * 60 >= difftime(current_time, shared_args->starting_time[local_line_index])) {
+            sleep(10);
 
-        // Update production speed for the current line
-        pthread_mutex_lock(&mutex);
-        shared_args->production_speed[local_line_index] = shared_args->produced_pills[local_line_index] - shared_args->pills_packaged[local_line_index];
-        pthread_mutex_unlock(&mutex);
-        printf("Speed of production line %d is %d\n", local_line_index, shared_args->production_speed[local_line_index]);
+            // Update production speed for the current line
+            pthread_mutex_lock(&mutex);
+            shared_args->production_speed[local_line_index] = shared_args->produced_pills[local_line_index] - shared_args->pills_packaged[local_line_index];
+            pthread_mutex_unlock(&mutex);
+            printf("Speed of production line %d is %d\n", local_line_index, shared_args->production_speed[local_line_index]);
+
+            for (int i = 0; i < shared_args->NUM_OF_PRODUCTION_LINES; i++) {
+                int min_speed_index = find_minimum_speed1();
+                printf("Minimum speed of line %d is %d\n", min_speed_index, shared_args->production_speed[min_speed_index]);
+
+                if (shared_args->production_speed[i] >= SWITCH_THRESHOLD) {
+                    kill(shared_args->production_line_pid[i], SIGUSR1);
+                    kill(shared_args->production_line_pid[min_speed_index], SIGUSR2);
+                    printf("Worker from line %d switched to line %d\n", min_speed_index, i);
+                    return NULL;
+                }
+            }
+        } else {
+            break;
+        }
     }
-
     return NULL;
 }
 
@@ -132,65 +164,118 @@ void signal_handler1(int signal) {
     }
 }
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s <number_of_lines>\n", argv[0]);
-        exit(EXIT_FAILURE);
+void create_new_thread1(int signal) {
+    pthread_t new_packager_worker;
+    if (pthread_create(&new_packager_worker, NULL, production_function1, &line_index) != 0) {
+        printf("Error in pthread create (packager)!\n");
+    }
+}
+
+void delete_thread1(int signal) {
+    srand(getpid() ^ time(NULL));
+
+    pthread_mutex_lock(&mutex);
+    shared_args->NUM_OF_WORKERS[production_line]--;
+    pthread_mutex_unlock(&mutex);
+}
+
+int main(int argc, char* argv[]) {
+    srand(getpid() ^ time(NULL));
+    if (argc != 3) {
+        printf("Usage: %s <line_index>\n", argv[0]);
+        return 1;
     }
 
-    int num_lines = atoi(argv[1]);
-    if (num_lines <= 0) {
-        fprintf(stderr, "Invalid number of lines: %d\n", num_lines);
-        exit(EXIT_FAILURE);
+   /* // Attach to shared memory
+    int shared_memory_id = shmget(SHM_KEY, SHM_SIZE, 0666);
+    if (shared_memory_id == -1) {
+        perror("shmget");
+        return 1;
     }
 
-    key_t shm_key = ftok("header.h", 65);
-    int shm_id = shmget(shm_key, sizeof(Shared_Argument), 0666 | IPC_CREAT);
-    shared_args = (Shared_Argument *)shmat(shm_id, NULL, 0);
-
-    // Initialize shared memory variables
-    shared_args->MAX_PILL_CONTAINERS_PER_LINE = 100;
-    shared_args->MAX_DELAY = 5;
-    shared_args->MIN_DELAY = 1;
-    shared_args->MAX_DELAY_FOR_INSPECTORS = 3;
-    shared_args->MIN_DELAY_FOR_INSPECTORS = 1;
-    shared_args->PACKAGING_DELAY_MAX = 5;
-    shared_args->PACKAGING_DELAY_MIN = 1;
-    shared_args->SIMULATION_DURATION = 60; // For example, run for 60 seconds
+    shared_args = (Shared_Argument *)shmat(shared_memory_id, NULL, 0);
+    if (shared_args == (void *)-1) {
+        perror("shmat");
+        return 1;
+    }
+*/
+    signal(SIGINT, signal_handler1);
+    signal(SIGUSR1, create_new_thread1);
+    signal(SIGUSR2, delete_thread1);
 
     pthread_mutex_init(&mutex, NULL);
 
-    pthread_t production_threads[num_lines];
-    pthread_t inspector_threads[num_lines];
-    pthread_t packager_threads[num_lines];
-    pthread_t manager_threads[num_lines];
-    int line_indices[num_lines];
+    line_index = atoi(argv[1]);
+    production_line = atoi(argv[1]);
+    shared_args->starting_time[production_line] = time(NULL);
 
-    signal(SIGINT, signal_handler1);
-
-    time_t current_time;
-    time(&current_time);
-
-    for (int i = 0; i < num_lines; i++) {
-        line_indices[i] = i;
-        shared_args->starting_time[i] = current_time;
-        pthread_create(&production_threads[i], NULL, production_function1, &line_indices[i]);
-        pthread_create(&inspector_threads[i], NULL, inspectors_function1, &line_indices[i]);
-        pthread_create(&packager_threads[i], NULL, packagers_function1, &line_indices[i]);
-        pthread_create(&manager_threads[i], NULL, manager_function1, &line_indices[i]);
+    // Create multiple inspector threads
+    pthread_t inspectors[shared_args->NUM_OF_WORKERS[production_line]];
+    for (int i = 0; i < shared_args->NUM_OF_WORKERS[production_line]; i++) {
+        if (pthread_create(&inspectors[i], NULL, inspectors_function1, &line_index) != 0) {
+            printf("Error in pthread create (inspectors)!\n");
+            return 1;
+        }
     }
 
-    for (int i = 0; i < num_lines; i++) {
-        pthread_join(production_threads[i], NULL);
-        pthread_join(inspector_threads[i], NULL);
-        pthread_join(packager_threads[i], NULL);
-        pthread_join(manager_threads[i], NULL);
+    // Create multiple packager threads
+    pthread_t packagers[shared_args->NUM_OF_WORKERS[production_line]];
+    for (int i = 0; i < shared_args->NUM_OF_WORKERS[production_line]; i++) {
+        if (pthread_create(&packagers[i], NULL, packagers_function1, &line_index) != 0) {
+            printf("Error in pthread create (packagers)!\n");
+            return 2;
+        }
+    }
+
+    // Create multiple production threads
+    pthread_t production[shared_args->NUM_OF_WORKERS[production_line]];
+    for (int i = 0; i < shared_args->NUM_OF_WORKERS[production_line]; i++) {
+        if (pthread_create(&production[i], NULL, production_function1, &line_index) != 0) {
+            printf("Error in pthread create (production)!\n");
+            return 3;
+        }
+    }
+
+    pthread_t manager;
+    if (pthread_create(&manager, NULL, manager_function1, &line_index) != 0) {
+        printf("Error in pthread create (manager)!\n");
+        return 7;
+    }
+
+    // Join all production threads
+    for (int i = 0; i < shared_args->NUM_OF_WORKERS[production_line]; i++) {
+        if (pthread_join(production[i], NULL) != 0) {
+            printf("Error in pthread join (production)!\n");
+            return 4;
+        }
+    }
+
+    // Join the manager thread
+    if (pthread_join(manager, NULL) != 0) {
+        printf("Error in pthread join (manager)!\n");
+        return 4;
+    }
+
+    // Allow inspectors and packagers to finish
+    sleep(2);
+
+    // Join all inspector threads
+    for (int i = 0; i < shared_args->NUM_OF_WORKERS[production_line]; i++) {
+        if (pthread_join(inspectors[i], NULL) != 0) {
+            printf("Error in pthread join (inspectors)!\n");
+            return 5;
+        }
+    }
+
+    // Join all packager threads
+    for (int i = 0; i < shared_args->NUM_OF_WORKERS[production_line]; i++) {
+        if (pthread_join(packagers[i], NULL) != 0) {
+            printf("Error in pthread join (packagers)!\n");
+            return 6;
+        }
     }
 
     pthread_mutex_destroy(&mutex);
-    shmdt(shared_args);
-    shmctl(shm_id, IPC_RMID, NULL);
-
     return 0;
 }
 
